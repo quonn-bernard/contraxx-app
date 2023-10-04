329842028b7f8319275edf44755efe7d77772775
Quonn S Bernard <quonn.bernard@gmail.com>
2023-10-04 18:54:17 -0400

    Adds login enpoint to API

--- backend/middleware/APIError.js
+++ backend/middleware/APIError.js
@@ -12,7 +12,23 @@ class INCOMPLETE_INPUT_ERROR extends Error {
   }
 }
 
+class WRONG_PASSWORD_ERROR extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name
+  }
+}
+
+class USER_DOES_NOT_EXIST_ERROR extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name
+  }
+}
+
 export {
     DUPLICATE_RESOURCE_ERROR,
-    INCOMPLETE_INPUT_ERROR
+    INCOMPLETE_INPUT_ERROR,
+    WRONG_PASSWORD_ERROR,
+    USER_DOES_NOT_EXIST_ERROR
 }
--- backend/services/user.js
+++ backend/services/user.js
@@ -3,11 +3,17 @@ import User from "../models/userModel.js";
 import * as dotenv from "dotenv";
 import { tokenGenerator } from "../utils/generators/tokenGenerator.js";
 import { hashedPWGenerator } from "../utils/generators/hashedPasswordGenerator.js";
-import { validateUserRegistrationInput } from "../utils/validation/inputValidation.js";
+import {
+  validateUserRegistrationInput,
+  validateUserLoginInput,
+} from "../utils/validation/inputValidation.js";
 import {
   DUPLICATE_RESOURCE_ERROR,
   INCOMPLETE_INPUT_ERROR,
+  WRONG_PASSWORD_ERROR,
+  USER_DOES_NOT_EXIST_ERROR,
 } from "../middleware/APIError.js";
+import bcrypt from "bcryptjs";
 
 dotenv.config();
 
@@ -25,16 +31,42 @@ const createUser = asyncHandler(async ({ fname, lname, email, password }) => {
 
   validateUserRegistrationInput({ fname, lname, email, password });
 
-    const newUser = await User.create({
-      fname,
-      lname,
-      email,
-      password: await hashedPWGenerator(password),
-    });
-    if (!newUser) throw new Error("Error while creating new user!");
+  const newUser = await User.create({
+    fname,
+    lname,
+    email,
+    password: await hashedPWGenerator(password),
+  });
 
-    return newUser
-  
+  return newUser;
 });
 
-export { createUser };
+const authenticateUser = asyncHandler(async ({ email, password }) => {
+  if (!email || !password) {
+    throw new INCOMPLETE_INPUT_ERROR("Email and password required!");
+  }
+
+  validateUserLoginInput({ email, password });
+
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    throw new USER_DOES_NOT_EXIST_ERROR(`No user with that email exists!`);
+  }
+
+  const result = await bcrypt.compare(password, user.password);
+
+  if (!result) {
+    throw new WRONG_PASSWORD_ERROR(
+      "Your email password combination doesn't match our records!"
+    );
+  }
+
+  return {
+    email,
+    password,
+    token: tokenGenerator(user.id),
+  };
+});
+
+export { createUser, authenticateUser };
