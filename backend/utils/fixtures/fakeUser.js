bb8e9d88a1e8319b84b4dd3690ef73d43069deb7
Quonn S Bernard <quonn.bernard@gmail.com>
2023-10-10 20:38:24 -0400

    Adds user login api endpoint and unit tests

--- backend/utils/fixtures/fakeUser.js
+++ backend/utils/fixtures/fakeUser.js
@@ -1,19 +1,60 @@
+const fname = "Albert";
+const correctEmail = "athomas@gmail.com";
+const wrongEmail = "frfrf@gmo.com";
+const correctPassword = "CoachThomas1!";
+const wrongPassword = "CoachThomas2!";
+const missingInput = "";
+
 const fakeuser = {
-    fname: "Albert",
-    lname: "Thomas",
-    email: "athomas@gmail.com",
-    password: "CoachThomas1!"
-}
+  fname: fname,
+  lname: "Thomas",
+  email: correctEmail,
+  password: correctPassword,
+};
 
 const incompleteUserInput = {
-    fname: "Albert",
-    lname: "",
-    email: "athomas@gmail.com",
-    password: "CoachThomas1!"
+  fname: "Albert",
+  lname: missingInput,
+  email: correctEmail,
+  password: correctPassword,
+};
+
+const missingEmailLogin = {
+  email: missingInput,
+  password: correctPassword,
+};
+
+const wrongEmailLogin = {
+  email: wrongEmail,
+  password: correctPassword,
+};
+
+const wrongPasswordLogin = {
+  email: correctEmail,
+  password: wrongPassword,
+};
+
+const missingPasswordLogin = {
+  email: correctEmail,
+  password: missingInput,
+};
+
+const correctLogin = {
+    email: correctEmail,
+    password: correctPassword
 }
 
 export {
-    fakeuser,
-    incompleteUserInput
-}
-
+  fakeuser,
+  incompleteUserInput,
+  missingEmailLogin,
+  missingPasswordLogin,
+  wrongEmailLogin,
+  missingInput,
+  wrongPassword,
+  wrongPasswordLogin,
+  correctEmail,
+  wrongEmail,
+  correctPassword,
+  correctLogin
+};
