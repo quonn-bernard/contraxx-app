2a2e05696cf8e8746ea21ad96441d859f5de3ec7
Quonn S Bernard <quonn.bernard@gmail.com>
2023-10-16 21:52:00 -0400

    Adds contract/archive endpoint to api

---
+++ backend/models/archivedContractModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const archivedContractSchema = mongoose.Schema({
+  fname: {
+    type: String,
+    required: [true, "Please add a first name"],
+  },
+  lname: {
+    type: String,
+    required: [true, "Please add a last name"],
+  },
+  eventtype: {
+    type: String,
+    required: [true, "Please add event type"],
+  },
+  rentalfeatures: {
+    type: Array,
+    required: [true],
+  },
+  bookingdate: {
+    type: String,
+    required: [true, "Date Required!"],
+  },
+  bookingtime: {
+    type: String,
+    required: [true, "Time Required!"],
+  },
+  bookingduration: {
+    type: Number,
+    min: 60,
+    required: [true, "Booking duration required!"],
+  },
+  bookingaddress: {
+    type: String,
+    required: [true, "Booking address required!"]
+  }
+});
+
+export default mongoose.model('ArchivedContract', archivedContractSchema)
--- backend/routes/contractRoutes.js
+++ backend/routes/contractRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const contractRouter = express.Router()
-import { createContract, updateContract } from "../controllers/contract.js";
+import { createContract, updateContract, archiveContract } from "../controllers/contract.js";
 import { tryCatch } from "../utils/tryCatch.js";
 
 contractRouter.post("/", tryCatch(createContract))
 contractRouter.put("/:id", tryCatch(updateContract))
+contractRouter.delete("/archive/:id", tryCatch(archiveContract))
 
 export default contractRouter
