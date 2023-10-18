6c90c2e4d293b3791725bfc853aef5db7a636852
Quonn S Bernard <quonn.bernard@gmail.com>
2023-10-18 18:39:56 -0400

    Adds unit test for contract updating method

--- backend/services/contract.js
+++ backend/services/contract.js
@@ -1,97 +1,101 @@
 import asyncHandler from "express-async-handler";
 import Contract from "../models/contractModel.js";
-import ArchivedContract from "../models/archivedContractModel.js"
+import ArchivedContract from "../models/archivedContractModel.js";
 import * as dotenv from "dotenv";
 import { ObjectId } from "mongodb";
-import {
-  INCOMPLETE_INPUT_ERROR,
-} from "../middleware/APIError.js";
+import { INCOMPLETE_INPUT_ERROR } from "../middleware/APIError.js";
 import { validateContractInput } from "../utils/validation/inputValidation.js";
 
 dotenv.config();
 
-const createNewContract = asyncHandler(async ({fname,
+const createNewContract = asyncHandler(
+  async ({
+    fname,
     lname,
     eventtype,
     rentalfeatures,
     bookingdate,
     bookingtime,
     bookingduration,
-    bookingaddress}) => {
+    bookingaddress,
+  }) => {
     const contractProperties = {
-        fname,
-        lname,
-        eventtype,
-        rentalfeatures,
-        bookingdate,
-        bookingtime,
-        bookingduration,
-        bookingaddress,
-      };
-  if (
-    !fname ||
-    !lname ||
-    !eventtype ||
-    !rentalfeatures ||
-    !bookingdate ||
-    !bookingtime ||
-    !bookingduration ||
-    !bookingaddress
-  ) {
-    throw new INCOMPLETE_INPUT_ERROR("Booking contract input is incomplete!");
+      fname,
+      lname,
+      eventtype,
+      rentalfeatures,
+      bookingdate,
+      bookingtime,
+      bookingduration,
+      bookingaddress,
+    };
+    if (
+      !fname ||
+      !lname ||
+      !eventtype ||
+      !rentalfeatures ||
+      !bookingdate ||
+      !bookingtime ||
+      !bookingduration ||
+      !bookingaddress
+    ) {
+      throw new INCOMPLETE_INPUT_ERROR("Booking contract input is incomplete!");
+    }
+
+    validateContractInput(contractProperties);
+
+    const newContract = await Contract.create(contractProperties);
+
+    return newContract;
   }
+);
 
-  validateContractInput(contractProperties);
-
-  const newContract = await Contract.create(contractProperties);
-
-  return newContract;
-});
-
-const updateAContract = asyncHandler(async (id, {fname,
-    lname,
-    eventtype,
-    rentalfeatures,
-    bookingdate,
-    bookingtime,
-    bookingduration,
-    bookingaddress}) => {
-  return await Contract.findByIdAndUpdate({ _id: id }, {fname,
-    lname,
-    eventtype,
-    rentalfeatures,
-    bookingdate,
-    bookingtime,
-    bookingduration,
-    bookingaddress}, {
-    new: true,
-  });
-});
+const updateAContract = asyncHandler(
+  async (
+    id,
+    input
+  ) => {
+    const newContract = await Contract.findByIdAndUpdate(
+      { _id: id },
+      input,
+      {
+        new: true,
+      }
+    );
+    return newContract
+  }
+);
 
 const archiveAContract = asyncHandler(async (existingContract) => {
-    const newDoc = Object.assign({}, existingContract);
-    delete newDoc._id;
-    newDoc.fname = existingContract.fname;
-    newDoc.lname = existingContract.lname;
-    newDoc.eventtype = existingContract.eventtype;
-    newDoc.rentalfeatures = existingContract.rentalfeatures;
-    newDoc.bookingdate = existingContract.bookingdate;
-    newDoc.bookingtime = existingContract.bookingtime;
-    newDoc.bookingduration = existingContract.bookingduration;
-    newDoc.bookingaddress = existingContract.bookingaddress;
-    newDoc._id = new ObjectId();
-    ArchivedContract.create(newDoc);
-    await Contract.deleteOne({ _id: existingContract._id });
-})
+  const newDoc = Object.assign({}, existingContract);
+  delete newDoc._id;
+  newDoc.fname = existingContract.fname;
+  newDoc.lname = existingContract.lname;
+  newDoc.eventtype = existingContract.eventtype;
+  newDoc.rentalfeatures = existingContract.rentalfeatures;
+  newDoc.bookingdate = existingContract.bookingdate;
+  newDoc.bookingtime = existingContract.bookingtime;
+  newDoc.bookingduration = existingContract.bookingduration;
+  newDoc.bookingaddress = existingContract.bookingaddress;
+  newDoc._id = new ObjectId();
+  ArchivedContract.create(newDoc);
+  await Contract.deleteOne({ _id: existingContract._id });
+});
 
 const retrieveAllContracts = asyncHandler(async () => {
-    const contracts = await Contract.find()
-    return contracts
-})
+  const contracts = await Contract.find();
+  return contracts;
+});
 
 const retrieveAllArchivedContracts = asyncHandler(async () => {
-    const contracts = await ArchivedContract.find()
-    return contracts
-})
+  const contracts = await ArchivedContract.find();
+  return contracts;
+});
 
-export { createNewContract, updateAContract, archiveAContract, retrieveAllContracts, retrieveAllArchivedContracts };
+export {
+  createNewContract,
+  updateAContract,
+  archiveAContract,
+  retrieveAllContracts,
+  retrieveAllArchivedContracts,
+};
--- backend/utils/fixtures/fakeContract.js
+++ backend/utils/fixtures/fakeContract.js
@@ -19,7 +19,19 @@ const incompleteContractInfo = {
     bookingduration: '180',
     bookingaddress: '4497 Ravenwood Dr. Union City, GA 30291'
 }
+
+const updateContractInfo = {
+    fname: "Coolio",
+    lname: "Bernard",
+    eventtype: "360",
+    rentalfeatures: ["props, custom overlay, online gallery"],
+    bookingdate: "2023-10-31",
+    bookingtime: '1800',
+    bookingduration: '180',
+    bookingaddress: '4497 Ravenwood Dr. Union City, GA 30291'
+}
 export {
     fakeContractInfo,
-    incompleteContractInfo
+    incompleteContractInfo,
+    updateContractInfo
 }
