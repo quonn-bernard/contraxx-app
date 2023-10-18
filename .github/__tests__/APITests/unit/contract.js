eec85a320a44621d898b37bf81f0ed0472b7fc9e
Quonn S Bernard <quonn.bernard@gmail.com>
2023-10-17 20:09:17 -0400

    Adds updated resource paths for contract.js unit tests

--- .github/__tests__/APITests/unit/contract.js
+++ .github/__tests__/APITests/unit/contract.js
@@ -1,9 +1,12 @@
 import {
   connectMemoryDB,
   dropMemoryDB,
-} from "../../../backend/utils/test-utils/mongoTestingDB.js";
-import { createNewContract } from "../../../backend/services/contract.js";
-import { incompleteContractInfo, fakeContractInfo } from "../../../backend/utils/fixtures/fakeContract.js";
+} from "../../../../backend/utils/test-utils/mongoTestingDB.js";
+import { createNewContract } from "../../../../backend/services/contract.js";
+import {
+  incompleteContractInfo,
+  fakeContractInfo,
+} from "../../../../backend/utils/fixtures/fakeContract.js";
 beforeAll(async () => {
   await connectMemoryDB();
 });
@@ -19,8 +22,8 @@ test("createNewContract function show throw 'Booking contract input is incomplet
 });
 
 test("createNewContract function should create new contract", async () => {
-    let newContract = await createNewContract(fakeContractInfo);
-    expect(newContract.fname).toEqual(fakeContractInfo.fname);
-    expect(newContract.rentalfeatures).toEqual(fakeContractInfo.rentalfeatures)
-    expect(newContract).toBeInstanceOf(Object)
-  });
+  let newContract = await createNewContract(fakeContractInfo);
+  expect(newContract.fname).toEqual(fakeContractInfo.fname);
+  expect(newContract.rentalfeatures).toEqual(fakeContractInfo.rentalfeatures);
+  expect(newContract).toBeInstanceOf(Object);
+});
